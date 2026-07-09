ca7727a1187802ea31fa6c4250d93c878eff18ee
Rohit Bhagat <rohit.d.bhagat01@gmail.com>
2026-07-09 12:35:48 +0530

    fix added

--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 }
 
 #Internet Gateway
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
@@ -91,6 +91,7 @@ resource "aws_route_table" "public"{
     vpc_id      = aws_vpc.main.id
 
     route {
+
         cidr_block      = "0.0.0.0/0"
         gateway_id      = aws_internet_gateway.igw.id
     }
